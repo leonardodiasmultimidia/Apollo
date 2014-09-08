d961015ee7e283e688657fa40cc5d7abac0957c8
Leonardo Dias <leonardo.dias@multimidiaeducacional.com.br>
2014-09-08 11:53:12 -0300

    Criação da entidade Coletanea

---
+++ src/Entidades/Coletanea.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Entidades;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Leonardo Dias
+ */
+public class Coletanea {
+    
+    private int id;
+    private ArrayList<Disco> coletanea;
+
+    public Coletanea(int id, ArrayList<Disco> coletanea) {
+        this.id = id;
+        this.coletanea = coletanea;
+    }
+
+    public Coletanea() {
+        id = -1;
+        coletanea = null;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public ArrayList<Disco> getColetanea() {
+        return coletanea;
+    }
+
+    public void setColetanea(ArrayList<Disco> coletanea) {
+        this.coletanea = coletanea;
+    }
+    
+    public int getNumDiscos(){
+        return coletanea.size();
+    }
+    
+    public void addDisco(Disco disco){
+        coletanea.add(disco);
+    }
+    
+    public void removeDisco(Disco disco){
+        coletanea.remove(disco);
+    }
+    
+    public void removeDiscoByPos(int pos){
+        coletanea.remove(pos);
+    }
+    
+    
+}
