84d80177c142cdef419bc5b19ae2b94f356330fd
Leonardo Dias <leonardo.dias@multimidiaeducacional.com.br>
2014-09-22 22:38:46 -0300

    Criação Reprodutor.java

---
+++ src/Player/Reprodutor.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Player;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javazoom.jl.decoder.JavaLayerException;
+
+/**
+ *
+ * @author Leonardo Dias
+ */
+public class Reprodutor {
+    
+    String path;
+    Player player;
+    Thread threadPlayer;
+    int frameAtual;
+    
+    public Reprodutor(){
+        path = null;
+        player = null;
+        threadPlayer = null;
+        frameAtual = -1;
+    }
+    
+    public void setMusica(String path){
+            this.path = path;
+    }
+    
+    public void play(){
+        try {
+            player = new Player(new FileInputStream(path), frameAtual);
+        } catch (JavaLayerException ex) {
+            Logger.getLogger(Reprodutor.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Reprodutor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        threadPlayer = new Thread(player);
+        threadPlayer.start();
+    }
+    
+    public void pause(){
+        frameAtual = player.pause();
+    }
+    
+}
