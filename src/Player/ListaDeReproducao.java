50824c212cf9169f9f43467716ef9cbc2964cbff
Leonardo Dias <leonardo.dias@multimidiaeducacional.com.br>
2014-09-22 23:22:07 -0300

    Correção ListaDeReproducao.java
    Correção em diversos métodos

--- src/Player/ListaDeReproducao.java
+++ src/Player/ListaDeReproducao.java
@@ -7,14 +7,9 @@
 package Player;
 
 import Entidades.Faixa;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Random;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javazoom.jl.decoder.JavaLayerException;
 
 /**
  *
@@ -27,11 +22,13 @@ public class ListaDeReproducao {
     private static Random random;
     private static Reprodutor reprodutor;
     private static Thread threadReprodutor;
+    private static boolean playing;
             
     public static void iniciaLista(){
         lista = new ArrayList<>();
         random = new Random(Calendar.getInstance().get(Calendar.MILLISECOND));
         faixaAtual = -1;
+        playing = false;
     }
     
     public static void insereFaixa(Faixa f){
@@ -60,40 +57,45 @@ public class ListaDeReproducao {
         }
         else faixaAtual++;
         if(faixaAtual>=lista.size()) faixaAtual = 0;
+        tocar();
     }
     
     public static void antMusica(){
         if(lista.size()==0)
             return;
         if(Configuracoes.getShuffle()){
-            faixaAtual = random.nextInt(lista.size());
+            int next;
+            do{
+            next = random.nextInt(lista.size());
+            }while(faixaAtual==next);
+            faixaAtual = next;
         }
         else faixaAtual--;
-        if(faixaAtual<0) faixaAtual = -1;
+        if(faixaAtual<0) faixaAtual = lista.size()-1;
+        tocar();
     }
     
     public static void tocar(){
         if(lista.size()!=0){
-            if(faixaAtual==-1)
+            if(faixaAtual==-1){
                 proxMusica();
+                return;
+            }            
         }
         else return;
-            reprodutor = new Reprodutor();
-            reprodutor.setMusica(lista.get(faixaAtual).getUrl()+".mp3");
-            reprodutor.play();
-        System.out.println("Tocando agora: "+lista.get(faixaAtual).getNome() + " - "+ lista.get(faixaAtual).getAutor()+" from: "+lista.get(faixaAtual).getUrl());
-    }
-    
-    public static void pausar(){
-            System.out.println("pausando...");
-            reprodutor.pause();
-            System.out.println("foi");
-    }
-    
-    public static void continuar(){
-        System.out.println("continuando...");
+        if(playing)
+            parar();
+        reprodutor = new Reprodutor();
+        reprodutor.setMusica(lista.get(faixaAtual).getUrl()+".mp3");
         reprodutor.play();
-        System.out.println("foi");
+        System.out.println("Tocando agora: "+lista.get(faixaAtual).getNome() + " - "+ lista.get(faixaAtual).getAutor()+" from: "+lista.get(faixaAtual).getUrl());
+        playing = true;
+    }
+    
+    public static void parar(){
+            System.out.println("parando...");
+            reprodutor.pause();
+            playing = false;
     }
     
 }
