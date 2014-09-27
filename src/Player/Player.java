9100ecb64d6826a4599ac1670cd17ca13dc46f7c
Leonardo Dias <leonardo.dias@multimidiaeducacional.com.br>
2014-09-27 03:28:21 -0300

    Correção Player.java

--- src/Player/Player.java
+++ src/Player/Player.java
@@ -6,44 +6,111 @@
 
 package Player;
 
-import java.io.FileInputStream;
+import java.io.File;
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javazoom.jl.decoder.JavaLayerException;
-import javazoom.jl.player.AudioDevice;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
 
 /**
  *
  * @author Leonardo Dias
  */
-public class Player extends javazoom.jl.player.advanced.AdvancedPlayer implements Runnable{
+public class Player implements Runnable{
+    private File file = null;
     
-    private int frameAtual;
-    
-    public Player(FileInputStream FIS, int frameAtual) throws JavaLayerException{
-        super(FIS);
-        this.frameAtual = frameAtual;
+    public Player(String file){
+        this.file = new File(file);
     }
     
-    public void resume(){
-        try{
-            if(frameAtual != -1)
-                play(frameAtual,10000);
-            else
-                play(0,1000);
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
+    public void play()
+    {
+      try
+      {
+        // Get AudioInputStream from given file.	
+        AudioInputStream in= AudioSystem.getAudioInputStream(file);
+        AudioInputStream din;
+        if (in != null)
+        {
+            AudioFormat baseFormat = in.getFormat();
+            AudioFormat  decodedFormat = new AudioFormat(
+                    AudioFormat.Encoding.PCM_SIGNED,
+                    baseFormat.getSampleRate(),
+                    16,
+                    baseFormat.getChannels(),
+                    baseFormat.getChannels() * 2,
+                    baseFormat.getSampleRate(),
+                    false);
+             // Get AudioInputStream that will be decoded by underlying VorbisSPI
+            din = AudioSystem.getAudioInputStream(decodedFormat, in);
+            // Play now !
+            rawplay(decodedFormat, din);
+            in.close();		
         }
+      }
+      catch (Exception e)
+      {
+        e.printStackTrace();
+      }
     }
-    
-    public void pause(){
-        
-        close();
+
+    private void rawplay(AudioFormat targetFormat, 
+                                       AudioInputStream din) throws IOException, LineUnavailableException
+    {
+       byte[] data = new byte[4096];
+      SourceDataLine line = getLine(targetFormat);		
+      if (line != null)
+      {
+         // Start
+        line.start();
+         int nBytesRead = 0, nBytesWritten = 0;
+         while (nBytesRead != -1)
+        {
+            if(Reprodutor.getStatus()==Reprodutor.PAUSED){
+                try {
+                    synchronized(this){
+                        wait();
+                    }
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Player.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            else if(Reprodutor.getStatus()==Reprodutor.STOPPED)
+                break;
+            nBytesRead = din.read(data, 0, data.length);
+            if (nBytesRead != -1) nBytesWritten = line.write(data, 0, nBytesRead);
+        }
+        line.drain();
+        line.stop();
+        line.close();
+        din.close();
+        if(Reprodutor.getStatus()!=Reprodutor.STOPPED){
+            if(Configuracoes.getRepeatState() == Configuracoes.REPETIR_UMA)
+                ListaDeReproducao.tocar();
+            else{
+                ListaDeReproducao.proxMusica();
+            }
+        }
+      }	
+
+}
+  private SourceDataLine getLine(AudioFormat audioFormat) throws LineUnavailableException
+    {
+      SourceDataLine res = null;
+      DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
+      res = (SourceDataLine) AudioSystem.getLine(info);
+      res.open(audioFormat);
+      return res;
     }
 
     @Override
     public void run() {
-        resume();
+        play();
     }
     
 }
