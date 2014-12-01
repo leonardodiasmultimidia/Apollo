ab542e60570653f9610eff05b708fd9552c7e769
Leonardo Dias <leonardo.dias@multimidiaeducacional.com.br>
2014-12-01 11:38:41 -0200

    Alteração de IUInsereFaixa

--- src/apollo/Graphics/IUInsereFaixa.java
+++ src/apollo/Graphics/IUInsereFaixa.java
@@ -6,6 +6,10 @@
 
 package apollo.Graphics;
 
+import apollo.BD.BD;
+import apollo.BD.Data;
+import apollo.Player.ListaDeReproducao;
+
 /**
  *
  * @author Leonardo Dias
@@ -16,6 +20,7 @@ public class IUInsereFaixa extends javax.swing.JFrame {
      * Creates new form IUInsereFaixa
      */
     public IUInsereFaixa() {
+        Data.recuperar();
         initComponents();
         setResizable(false);
         setVisible(true);
@@ -55,8 +60,9 @@ public class IUInsereFaixa extends javax.swing.JFrame {
         jButtonBuscar = new javax.swing.JButton();
         jRadioButtonNome = new javax.swing.JRadioButton();
         jRadioButtonAutor = new javax.swing.JRadioButton();
+        jButtonInserir = new javax.swing.JButton();
         scrollPaneFaixas = new java.awt.ScrollPane();
-        jPanel1 = new javax.swing.JPanel();
+        jPanelFaixas = new apollo.Graphics.JPanelFaixas();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -82,7 +88,18 @@ public class IUInsereFaixa extends javax.swing.JFrame {
 
         jLanelBuscar.setText("Buscar");
 
+        jTextFieldBuscar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextFieldBuscarActionPerformed(evt);
+            }
+        });
+
         jButtonBuscar.setText("ir");
+        jButtonBuscar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonBuscarActionPerformed(evt);
+            }
+        });
 
         jRadioButtonNome.setBackground(new java.awt.Color(250, 250, 250));
         jRadioButtonNome.setText("Nome");
@@ -95,13 +112,20 @@ public class IUInsereFaixa extends javax.swing.JFrame {
         jRadioButtonAutor.setBackground(new java.awt.Color(250, 250, 250));
         jRadioButtonAutor.setText("Autor");
 
+        jButtonInserir.setText("Inserir");
+        jButtonInserir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonInserirActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanelOptionsLayout = new javax.swing.GroupLayout(jPanelOptions);
         jPanelOptions.setLayout(jPanelOptionsLayout);
         jPanelOptionsLayout.setHorizontalGroup(
             jPanelOptionsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanelOptionsLayout.createSequentialGroup()
-                .addGroup(jPanelOptionsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(jPanelOptionsLayout.createSequentialGroup()
+                .addGroup(jPanelOptionsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelOptionsLayout.createSequentialGroup()
                         .addGap(0, 0, Short.MAX_VALUE)
                         .addComponent(jSeparator2, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -110,13 +134,13 @@ public class IUInsereFaixa extends javax.swing.JFrame {
                         .addGroup(jPanelOptionsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(jSeparator4, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 179, Short.MAX_VALUE)
                             .addComponent(jSeparator1, javax.swing.GroupLayout.Alignment.TRAILING)))
-                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanelOptionsLayout.createSequentialGroup()
-                        .addGroup(jPanelOptionsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanelOptionsLayout.createSequentialGroup()
+                    .addGroup(jPanelOptionsLayout.createSequentialGroup()
+                        .addGroup(jPanelOptionsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanelOptionsLayout.createSequentialGroup()
                                 .addComponent(jSeparator3, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(jLanelBuscar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanelOptionsLayout.createSequentialGroup()
+                            .addGroup(jPanelOptionsLayout.createSequentialGroup()
                                 .addContainerGap()
                                 .addGroup(jPanelOptionsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(jRadioButtonFaixas)
@@ -125,15 +149,17 @@ public class IUInsereFaixa extends javax.swing.JFrame {
                                     .addGroup(jPanelOptionsLayout.createSequentialGroup()
                                         .addComponent(jTextFieldBuscar, javax.swing.GroupLayout.PREFERRED_SIZE, 228, javax.swing.GroupLayout.PREFERRED_SIZE)
                                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                        .addComponent(jButtonBuscar, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                                        .addComponent(jButtonBuscar, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                            .addGroup(jPanelOptionsLayout.createSequentialGroup()
+                                .addGap(10, 10, 10)
+                                .addGroup(jPanelOptionsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(jRadioButtonNome, javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jRadioButtonAutor, javax.swing.GroupLayout.Alignment.LEADING)))
+                            .addGroup(jPanelOptionsLayout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(jButtonInserir)))
                         .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
-            .addGroup(jPanelOptionsLayout.createSequentialGroup()
-                .addGap(10, 10, 10)
-                .addGroup(jPanelOptionsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jRadioButtonNome, javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jRadioButtonAutor, javax.swing.GroupLayout.Alignment.LEADING))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanelOptionsLayout.setVerticalGroup(
             jPanelOptionsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -164,40 +190,30 @@ public class IUInsereFaixa extends javax.swing.JFrame {
                 .addGroup(jPanelOptionsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jTextFieldBuscar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButtonBuscar))
-                .addContainerGap(186, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addComponent(jButtonInserir)
+                .addContainerGap(145, Short.MAX_VALUE))
         );
 
-        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 504, Short.MAX_VALUE)
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 416, Short.MAX_VALUE)
-        );
-
-        scrollPaneFaixas.add(jPanel1);
+        scrollPaneFaixas.add(jPanelFaixas);
 
         javax.swing.GroupLayout jPanelFundoLayout = new javax.swing.GroupLayout(jPanelFundo);
         jPanelFundo.setLayout(jPanelFundoLayout);
         jPanelFundoLayout.setHorizontalGroup(
             jPanelFundoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelFundoLayout.createSequentialGroup()
-                .addComponent(scrollPaneFaixas, javax.swing.GroupLayout.DEFAULT_SIZE, 503, Short.MAX_VALUE)
+                .addComponent(scrollPaneFaixas, javax.swing.GroupLayout.PREFERRED_SIZE, 503, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, 0)
                 .addComponent(jPanelOptions, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         jPanelFundoLayout.setVerticalGroup(
             jPanelFundoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanelFundoLayout.createSequentialGroup()
-                .addGroup(jPanelFundoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jPanelOptions, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(scrollPaneFaixas, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(jPanelOptions, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, Short.MAX_VALUE))
+            .addGroup(jPanelFundoLayout.createSequentialGroup()
+                .addComponent(scrollPaneFaixas, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(0, 0, 0))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -223,6 +239,41 @@ public class IUInsereFaixa extends javax.swing.JFrame {
     private void jRadioButtonNomeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButtonNomeActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jRadioButtonNomeActionPerformed
+
+    private void jTextFieldBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextFieldBuscarActionPerformed
+        jButtonBuscarActionPerformed(evt);                
+    }//GEN-LAST:event_jTextFieldBuscarActionPerformed
+
+    private void jButtonBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonBuscarActionPerformed
+        if (jRadioButtonNome.isSelected()) {
+            if (jRadioButtonFaixas.isSelected()){
+                jPanelFaixas.addFaixas(BD.getAllFaixaByNome(jTextFieldBuscar.getText()));
+            }
+            else if (jRadioButtonDiscos.isSelected()){
+                
+            }
+            else{
+                
+            }
+        } else {
+            if (jRadioButtonFaixas.isSelected()){ 
+                jPanelFaixas.addFaixas(BD.getAllFaixaByAutor(jTextFieldBuscar.getText()));
+            }
+            else if (jRadioButtonDiscos.isSelected()){
+                
+            }
+            else{
+                
+            }
+        }
+        jPanelFaixas.validate();
+        jPanelFaixas.repaint();
+    }//GEN-LAST:event_jButtonBuscarActionPerformed
+
+    private void jButtonInserirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonInserirActionPerformed
+        ListaDeReproducao.insereOutraLista(jPanelFaixas.getSelected());
+        dispose();
+    }//GEN-LAST:event_jButtonInserirActionPerformed
 
     /**
      * @param args the command line arguments
@@ -263,8 +314,9 @@ public class IUInsereFaixa extends javax.swing.JFrame {
     private javax.swing.ButtonGroup buttonGroupBuscar;
     private javax.swing.ButtonGroup buttonGroupCategoria;
     private javax.swing.JButton jButtonBuscar;
+    private javax.swing.JButton jButtonInserir;
     private java.awt.Label jLanelBuscar;
-    private javax.swing.JPanel jPanel1;
+    private apollo.Graphics.JPanelFaixas jPanelFaixas;
     private javax.swing.JPanel jPanelFundo;
     private javax.swing.JPanel jPanelOptions;
     private javax.swing.JRadioButton jRadioButtonAutor;
@@ -280,4 +332,5 @@ public class IUInsereFaixa extends javax.swing.JFrame {
     private java.awt.Label label1;
     private java.awt.ScrollPane scrollPaneFaixas;
     // End of variables declaration//GEN-END:variables
+
 }
